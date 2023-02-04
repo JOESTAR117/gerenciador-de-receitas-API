4ffe9722f4487a2cdace6f25afb22396d2b45eb5
Jackson Rios <juniorrios1231@gmail.com>
2023-02-04 17:10:39 -0300

    feat: router and controller create user finished✔

--- src/controllers/AuthRegisterUserController.js
+++ src/controllers/AuthRegisterUserController.js
@@ -1,7 +1,60 @@
-import User from "../models/User";
+import User from '../models/User'
+import bcrypt from 'bcrypt'
 
 export class AuthRegisterUserController {
-    static async init(req,res){
-        res.send({message: 'welcome to API'})
+    static async init(req, res) {
+        res.send({ message: 'welcome to API' })
+    }
+
+    static async registerUser(req, res) {
+        const { name, email, age, password, confirmPassword } = req.body
+
+        let image = ''
+
+        if (req.file) {
+            image = req.file.filename
+        }
+        if (!name || !email || !age || !password) {
+            res.status(400).json({
+                message: 'Submit all fields for registration',
+            })
+        }
+        if (password != confirmPassword) {
+            return res
+                .status(422)
+                .json({ message: 'passwords are not the same' })
+        }
+
+        const userExist = await User.findOne({ email: email })
+
+        if (userExist) {
+            return res.status(422).json({ message: 'Email already exists' })
+        }
+
+        const hash = await bcrypt.genSalt(10)
+
+        const passwordIsValid = await bcrypt.hash(password, hash)
+
+        const user = new User({
+            name,
+            email,
+            age,
+            image,
+            password: passwordIsValid,
+        })
+        try {
+            await user.save()
+            res.status(201).json({
+                message: 'User created successfully',
+                user: {
+                    id: user._id,
+                    name,
+                    email,
+                    age,
+                },
+            })
+        } catch (err) {
+            console.log(err.message)
+        }
     }
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -21,11 +20,6 @@ const UserSchema = new mongoose.Schema({
         required: true,
         select: false,
     },
-})
-
-UserSchema.pre('save', async function (next) {
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
 })
 
 const User = mongoose.model('User', UserSchema)
