2ee4cdad372fd8a5adc144fbd22f65e6546d43a2
Jackson Rios <juniorrios1231@gmail.com>
2023-02-04 15:30:40 -0300

    feat: mongoDbAtlass connected✔

---
+++ src/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const connectDatabase = () => {
+    console.log('wait connecting to the database')
+
+    mongoose
+        .connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => console.log('MongoDB atlas Connectd'))
+        .catch((error) => console.error(error.message))
+}
+export default connectDatabase
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDatabase from './database/db'
+
+dotenv.config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+connectDatabase()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
